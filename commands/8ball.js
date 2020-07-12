87c86b8ecab6700929cd2bda1081133906370934
RedstonyTony <38585570+RedstonyTony@users.noreply.github.com>
2020-07-12 20:55:56 +0100

    8ball is now smarter
    The title says it all

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,38 +1,45 @@
-const possibleAnswers = [
-  "wut",
-  "yes",
-  "no",
-  "..no u",
-  "idk",
-  "No",
-  "Yes",
-  "uhh",
-  "i dont think so m8",
-  "tHe AnSwEr LiEs WiThIn",
-  "wait wot",
-  "Why don't you ask magic8ball instead"
-];
 exports.run = async (client, message, args) => {
-  // eslint-disable-line no-unused-vars
-  try {
-    if (args[0]) {
-      if (message.content.endsWith("?")) {
-        message.channel.send(possibleAnswers.random());
-      } else return message.reply("A question ends with a question mark.");
-    } else return message.reply("You must provide a question!");
-  } catch (err) {
-    message.channel.send("There was an error!\n" + err).catch();
-  };
+    if (!args[0]) return message.reply('You shall give the mighty 8ball a question.');
+    const outcomes = [
+        "It's certain",
+        "It's decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"
+    ];
+    if (message.content.endsWith("?")) {
+        const embed = new client.discord.RichEmbed()
+            .setColor("#07a702")
+            .setTitle('According to the 8Ball')
+            .setDescription(`🎱 ${outcomes.random()}`)
+            .setFooter(`Executed by ${message.author.username}`, message.author.displayAvatarURL);
+        message.channel.send(embed);
+    } else return message.reply("The mighty 8ball denies inadequate syntax.");
 };
 exports.conf = {
-  enabled: true,
-  aliases: ["8b"],
-  guildOnly: false,
-  permLevel: "User"
+    enabled: true,
+    guildOnly: false,
+    aliases: ["8bll", "8b", "8ball"],
+    permLevel: "User"
 };
 exports.help = {
-  name: "8ball",
-  category: "Social",
-  description: "Returns the answer to your question",
-  usage: "8ball <question>"
+    name: "8ball",
+    category: "Social",
+    description: "Ask it anything! It won't bite.",
+    usage: "8ball <question>"
 };
