6dc5e23736b51b0cfaa91bc9267550aabde0ed4b
RedstonyTony <38585570+RedstonyTony@users.noreply.github.com>
2020-07-12 21:20:41 +0100

    Baka command
    Stupid

---
+++ commands/baka.js
@@ -0,0 +1,24 @@
+exports.run = async (client, message) => { // eslint-disable-line no-unused-vars
+  try {
+    const baka = await client.nekoslife.sfw.baka();
+    const embed = new client.discord.RichEmbed()
+      .setColor("#07a702")
+      .setTitle("Baka :face_palm:")
+      .setImage(baka.url);
+    message.channel.send(embed);
+  } catch (err) {
+    message.channel.send("There was an error!\n" + err).catch();
+  };
+};
+exports.conf = {
+  enabled: true,
+  aliases: ["animebaka"],
+  guildOnly: false,
+  permLevel: "User"
+};
+exports.help = {
+  name: "baka",
+  category: "Photographic",
+  description: "Returns a stupidity-comprehension-tension anime baka.",
+  usage: "baka"
+};
